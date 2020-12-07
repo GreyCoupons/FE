c70dd8ac54e738ed83884d5a8f1f922aa2a18db0
Miguel Diaz <diaz4674@gmail.com>
2020-12-07 22:50:52 +0000

    updated mobile height

--- src/views/homepage/style/portrait.js
+++ src/views/homepage/style/portrait.js
@@ -1,7 +1,7 @@
 export default `
     .mobileView {
         display: block;
-        height: 100vh;
+        height: 100%;
         overflow: scroll;
         overflow-x: hidden;
         -webkit-overflow-scrolling: touch;
@@ -10,14 +10,13 @@ export default `
     }
 
     .mobileView::-webkit-scrollbar {
-  display: none;
-}
-
-/* Hide scrollbar for IE, Edge and Firefox */
-.mobileView {
-  -ms-overflow-style: none;  /* IE and Edge */
-  scrollbar-width: none;  /* Firefox */
-}
+        display: none;
+    }
+    /* Hide scrollbar for IE, Edge and Firefox */
+    .mobileView {
+        -ms-overflow-style: none;  /* IE and Edge */
+        scrollbar-width: none;  /* Firefox */
+    }
     .viewheight {
         display: none;
     }
