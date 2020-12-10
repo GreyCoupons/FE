1544c524dd2f3153e1ef5f7caaa1e78dfb75a52b
Miguel Diaz <diaz4674@gmail.com>
2020-12-10 17:55:42 +0000

    yellow bar style

--- src/components/splashImg/style/landscape.js
+++ src/components/splashImg/style/landscape.js
@@ -7,13 +7,13 @@ export default `
     .yellowBar {
         position: absolute;
         width: 100%;
-        height: 300px;
+        height: 255px;
         background: #E1C972;
         box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.15);
         transition: .3s ease;
         
         @media (max-width: 1135px) {
-            height: 250px;
+            height: 200px;
         }    
     }
     .splash {
