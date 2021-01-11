aeda91a73cc1d849f9cf576d144613622865f043
Miguel Diaz <diaz4674@gmail.com>
2021-01-11 23:08:50 +0000

    added error message

--- src/components/featured/index.js
+++ src/components/featured/index.js
@@ -14,45 +14,45 @@ import Loading from "../Loading"
 import Style from "./style"
 
 class DealofDay extends Component {
-	state = {
-		featuredCoupons: [],
-	}
-	async componentDidMount() {
-		await axios
-			.post("https://greycoupon-test.herokuapp.com/get/featured")
-			.then((coupons) => {
-				this.setState({ featuredCoupons: coupons.data })
-			})
-			.catch((error) => {
-				// handle error
-				console.log(error)
-			})
-	}
-	render() {
-		return (
-			<Style className="page search">
-				<div className="main">
-					{this.state.featuredCoupons.length === 0 ? (
-						<Loading />
-					) : (
-						<>
-							<SplashImg />
-							<SearchBar />
-							<div id="featuredSection">
-								<div className="row">
-									<p className="title">Featured</p>
-									<img src={Star} alt="star" className="star" />
-								</div>
-								<FeaturedProducts
-									displayProducts={this.state.featuredCoupons}
-								/>
-							</div>
-						</>
-					)}
-				</div>
-			</Style>
-		)
-	}
+    state = {
+        featuredCoupons: [],
+    }
+    async componentDidMount() {
+        await axios
+            .post("https://greycoupon-test.herokuapp.com/get/featured")
+            .then((coupons) => {
+                this.setState({ featuredCoupons: coupons.data })
+            })
+            .catch((error) => {
+                // handle error
+                console.log(error, "sorry something went wrong.")
+            })
+    }
+    render() {
+        return (
+            <Style className="page search">
+                <div className="main">
+                    {this.state.featuredCoupons.length === 0 ? (
+                        <Loading />
+                    ) : (
+                            <>
+                                <SplashImg />
+                                <SearchBar />
+                                <div id="featuredSection">
+                                    <div className="row">
+                                        <p className="title">Featured</p>
+                                        <img src={Star} alt="star" className="star" />
+                                    </div>
+                                    <FeaturedProducts
+                                        displayProducts={this.state.featuredCoupons}
+                                    />
+                                </div>
+                            </>
+                        )}
+                </div>
+            </Style>
+        )
+    }
 }
 
 export default DealofDay
